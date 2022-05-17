78ff5dcd57adf9b6ce471476affc03d003b9090a
Retype GitHub Action <retype@object.net>
2022-05-17 22:50:31 +0000

    Refreshes Retype-generated documentation.
    Process triggered by cxmain.

---
+++ static/scripts/whitepaper.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pandoc ../docs/other/whitepaper.md ../docs/blockchain/overview.md ../docs/cubics/about.md ../docs/cubics/focus.md ../docs/cubics/tokenomics.md ../docs/cubics/roadmap.md ../docs/cubics/resources.md -o ../dist/whitepaper.pdf
